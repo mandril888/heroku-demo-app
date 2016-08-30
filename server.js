9aa8de3ceff689112d63f8e49d65561ce6fd4cb6
Arturo Rubio <arturo.rubio.89.8@gmail.com>
2016-08-30 12:59:26 +0200

    second commit

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-const express = require('express');
+var express = require('express');
+var app = express();
 
-let app = express()
-
-app.get('/', (req,res) => {
+app.get('/', function (req,res) {
 	res.send("Welcome to my app");
 })
 
-app.listen(3000, () => console.log("Listening on port 3000"))
+app.listen(3000, function() {
+	console.log("Listening on port 3000")
+});
