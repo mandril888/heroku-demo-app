93838a3a9705839dc85074c3df36cdbf766437c7
Arturo Rubio <arturo.rubio.89.8@gmail.com>
2016-08-30 13:19:58 +0200

    upload port of the server

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
+var PORT = process.env.PORT || 3000;
 
 app.get('/', function (req,res) {
 	res.send("Welcome to my app");
 })
 
 app.listen(3000, function() {
-	console.log("Listening on port 3000")
+	console.log("Listening on port " + PORT)
 });
